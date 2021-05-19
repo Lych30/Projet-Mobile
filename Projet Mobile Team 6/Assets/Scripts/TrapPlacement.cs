399b76a8860ce6c7c6a52d4d61e420e22d5c3fe3
Leo <Aqu4men@gmail.com>
2021-05-19 13:22:39 +0200

    optimisation des déplacements des traps 2e editions

--- Projet Mobile Team 6/Assets/Scripts/TrapPlacement.cs
+++ Projet Mobile Team 6/Assets/Scripts/TrapPlacement.cs
@@ -7,23 +7,25 @@ public class TrapPlacement : MonoBehaviour
 {
     private void OnMouseDrag()
     {
+        gameObject.layer = LayerMask.NameToLayer("Default");
         transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         if (!AstarPath.active.GetNearest(transform.position).node.Walkable)
         {
-            GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 255);
+            GetComponent<SpriteRenderer>().color = new Color(0.5f, 0, 0, 0.5f);
         }
         else
         {
-            GetComponent<SpriteRenderer>().color = new Color(0, 255, 0, 255);
+            GetComponent<SpriteRenderer>().color = new Color(0, 0.5f, 0, 0.5f);
         }
     }
     private void OnMouseUp()
     {
         GraphNode node = AstarPath.active.GetNearest(transform.position, NNConstraint.Default).node;
         transform.position = (Vector3)node.position;
-        GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
         Camera.main.GetComponent<zoomPinch>().enabled = true;
+        gameObject.layer = LayerMask.NameToLayer("Obstacles");
     }
     private void OnMouseDown()
     {
