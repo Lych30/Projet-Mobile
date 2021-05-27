98dd085facb962a510db94b197bc1d72a1114ce7
Moony4012 <42449752+Moony4012@users.noreply.github.com>
2021-05-27 19:39:57 +0200

    REPUSH Raph

---
+++ Projet Mobile Team 6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            var TouchPos = Camera.main.ScreenPointToRay(touch.position);
+
+            RaycastHit2D hit = Physics2D.Raycast(TouchPos.origin,TouchPos.direction,Mathf.Infinity);
+            if (hit)
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.transform.CompareTag("Trap"))
+                {
+                    hit.transform.GetComponent<TrapTest>().enabled = true;
+                    
+                }
+            }
+        }
+    }
+}
